104849a2653b460e295c2084b3f4a88591407575
ElizabethBeaton <elizabethbeaton23@outlook.com>
2023-04-30 22:10:25 +0100

    created footer, reservation and email signup page.
    Also sorted out a lot of the css to make this as identical to original website.

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+const navbar = document.getElementById("navbar");
+const menu = document.getElementById("menu");
+
+window.onscroll = function () {
+  if (window.pageYOffset >= menu.offsetTop) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+};*/
+/*
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll("nav a").forEach((link) => {
+  if (link.href.includes(`${activePage}`)) {
+    link.classList.add("active");
+  }
+});
+*/
