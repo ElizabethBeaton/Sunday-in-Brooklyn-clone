c055cfb6c66c9b235fbb5f30e6567ca2e3148631
ElizabethBeaton <elizabethbeaton23@outlook.com>
2023-05-03 18:53:04 +0100

    added a footer, javascript for navbar, and background images on pages
    also made further changes to css to improve similarity

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-/*
-const navbar = document.getElementById("navbar");
-const menu = document.getElementById("menu");
+const navbar = document.getElementById("main-navbar");
+const menu = document.getElementById("main-menu");
 
 window.onscroll = function () {
   if (window.pageYOffset >= menu.offsetTop) {
@@ -8,12 +7,6 @@ window.onscroll = function () {
   } else {
     navbar.classList.remove("sticky");
   }
-};*/
-/*
-const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll("nav a").forEach((link) => {
-  if (link.href.includes(`${activePage}`)) {
-    link.classList.add("active");
-  }
-});
-*/
+};
+
+
