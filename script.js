21a68aa95cc0c2255f91e3cd6070a932ab52c92c
ElizabethBeaton <elizabethbeaton23@outlook.com>
2023-05-10 22:28:53 +0100

    changed images for press and index page

--- script.js
+++ script.js
@@ -4,11 +4,3 @@ const navbarLinks = document.querySelector(".navbar-links");
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
 });
-
-/*
-const toggleButtonBars = document.querySelector(".toggle-button");
-
-toggleButtonBars.addEventListener("click", () => {
-  toggleButtonBars.classList.toggle("active");
-});
-*/
